cf04ed4e92ae643167a75ab476cea0ff44af4168
tuliang <info@tuliang.org>
2013-04-01 19:20:45 +0800

    创建演示程序

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3db9affc62d0745d63c74d670f474e8eb1e061b2d35b9f9149adf4ec5662423c33bf02f8eef95897c194f5e1b6da21461c509a816b300e2788c725827f47ac75'
