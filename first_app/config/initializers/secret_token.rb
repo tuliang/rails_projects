50078a3e5471765e3b2f6f5e674bdf52226b76bd
tuliang <info@tuliang.org>
2013-04-01 18:37:10 +0800

    运行 rails 生成一个新程序

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'aff383a20af07cdff6786503399d71d252f8d71d0ce715954a2877053424dfbd54e6f51c58cb757ecc6872f5b7917678dfc3b488c706c0f5d55cf42da9686186'
